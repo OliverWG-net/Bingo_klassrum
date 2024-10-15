e151db78771dedfcc93aa65710b2194a26e020a0
Oliver <Oliver.garderud@chasacademy.se>
2024-10-15 10:37:08 +0200

    Testing

--- Program.cs
+++ Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.
+                Console.WriteLine("Hello, World!"); Console.WriteLine("Hello, World!");
+
         }
         
 	}
